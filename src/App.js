96e1daf938af2c88f5153cf70a459a4eba00b1f1
TylerWilliams97 <w.tyler1997@gmail.com>
2021-03-30 22:29:02 -0700

    added api call

---
+++ src/App.js
@@ -0,0 +1,25 @@
+//import './App.css';
+import Employees from './API'
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        
+ 
+         
+        <Employees></Employees>
+        
+       
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
